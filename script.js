3f002e2cfe49a3eced6264dbf4506226eb1e4d86
byZeet <cosminrv98@gmail.comm>
2023-02-25 00:39:29 +0100

    NavBard + Hero (Sin cambios significativos)

---
+++ script.js
@@ -0,0 +1,9 @@
+alert ('Test JS')
+
+const isScrolling = () => {
+    const headerEl = document.querySelector('header')
+    let windowPosition = window.scrollY > 250 
+    headerEl.classList.toggle('active', windowPosition)
+}
+
+window.addEventListener('scroll', isScrolling)
